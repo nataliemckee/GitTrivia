757e7762dfa6cf997063d9ae64c0c442099b10a4
nsmckee <nsmckee@umich.edu>
2019-10-27 12:47:40 -0400

    did this work

---
+++ Mrover_practice/Mrover_practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mrover_practice
+//
+//  Created by Natalie McKee on 10/27/19.
+//  Copyright © 2019 Natalie McKee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
